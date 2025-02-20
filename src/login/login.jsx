ee607f839e6b4c48891e0a891c96aba089af6435
Talmage Woodhouse <tw456@byu.edu>
2025-02-19 17:37:34 -0700

    some css width stuff and footer spacing done

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,17 +1,18 @@
 import React from "react";
+import "./login.css";
 
 export function Login() {
   return (
     <main>
       <h1>Login to play</h1>
       <form method="get" action="play.html">
-        <div style="width: coopx; margin: auto; text-align: center">
+        <div className="form-container">
           <input type="text" placeholder="your@email.com" />
         </div>
-        <div style="width: coopx; margin: auto; text-align: center">
+        <div className="form-container">
           <input type="password" placeholder="password" />
         </div>
-        <div style="text-align: center">
+        <div>
           <button type="submit">Login</button>
           <button type="submit">Create</button>
         </div>
